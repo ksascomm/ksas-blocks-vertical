449ac3c71b50c6a18ae1af163b92effd08c5352a
ksascomm <ksascomm@gmail.com>
2021-06-14 16:01:27 -0400

    Add modified date meta

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="date" content="<?php the_modified_date(); ?>" />
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<title><?php create_page_title(); ?></title>
 	<?php wp_head(); ?>

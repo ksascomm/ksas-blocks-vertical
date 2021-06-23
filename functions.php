d2f8516f971d460eb75c23c1bd132d28e5eaf237
ksascomm <ksascomm@gmail.com>
2021-06-23 09:58:55 -0400

    Load child theme css after parent theme

--- functions.php
+++ functions.php
@@ -14,19 +14,13 @@ add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
 	 * Note that this function is hooked into the wp_enqueue_scripts
 	 */
 function child_theme_enqueue_styles() {
-	$parenthandle = 'parent-style'; // This is 'ksas-blocks-style-css' for the KSAS Blocks theme.
-	$theme        = wp_get_theme();
-	wp_enqueue_style(
-		$parenthandle,
-		get_template_directory_uri() . '/style.css',
-		array(),  // if the parent theme code has a dependency, copy it to here.
-		$theme->parent()->get( 'Version' )
-	);
+	$parent_style = 'ksas-blocks-style';
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/dist/css/style.css', array(), filemtime( get_template_directory() . '/resources/css' ), 'all' );
 	wp_enqueue_style(
 		'child-style',
-		get_stylesheet_uri(),
-		array( $parenthandle ),
-		$theme->get( 'Version' ) // this only works if you have Version in the style header.
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
 	);
 
 }

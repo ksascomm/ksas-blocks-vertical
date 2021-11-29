5d34b1eb4ee6804a99cc0d24daa731b0d6221595
ksascomm <ksasweb@jhu.edu>
2021-11-29 10:47:02 -0500

    Add Google Tag Manager code

--- header.php
+++ header.php
@@ -37,6 +37,13 @@
 		<?php endif; ?>
 	</script>
 	<!-- End Google Analytics -->
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5VTN64C');</script>
+	<!-- End Google Tag Manager -->
 	<link rel="preload" href="<?php echo esc_url( get_template_directory_uri() . '/dist/fonts/gentona/Gentona-Light.woff' ); ?>" as="font" type="font/woff" crossorigin="anonymous">
 	<link rel="preload" href="<?php echo esc_url( get_template_directory_uri() . '/dist/fonts/gentona/Gentona-SemiBold.woff' ); ?>" as="font" type="font/woff" crossorigin="anonymous">
 	<link rel="preload" href="<?php echo esc_url( get_template_directory_uri() . '/dist/fonts/gentona/Gentona-Bold.woff' ); ?>" as="font" type="font/woff" crossorigin="anonymous">
